6c199aa84d1bccde288dd4db440316318129cc39
reachtosuresh <67258122+reachtosuresh@users.noreply.github.com>
2020-06-22 00:23:52 -0700

    weather-report
    weather-report

---
+++ weather-report/src/com/weather/Forecast.java
@@ -0,0 +1,16 @@
+package com.weather;
+
+public class Forecast {
+	
+	private ForecastProperties properties;
+
+	public ForecastProperties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(ForecastProperties properties) {
+		this.properties = properties;
+	}
+	
+
+}
